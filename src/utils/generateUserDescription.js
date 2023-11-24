2bfd35c02796c980ffde3668e086c292637fbcc3
GRdevelops <gianniruggieri99@gmail.com>
2023-11-24 16:07:55 +0100

    heroku small fix

--- src/utils/generateUserDescription.js
+++ src/utils/generateUserDescription.js
@@ -28,7 +28,8 @@ const generateUserDescription = async (userData, setUserDescription) => {
 
     console.log('prompt:', prompt);
 
-    const response = await axios.post("http://localhost:8000/generate-workout", {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+    const response = await axios.post(`${backendUrl}/generate-workout`, {
       message: prompt,
     });
 

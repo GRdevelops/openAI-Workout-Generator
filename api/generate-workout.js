48e2e1dafc17d99e164af948dc6755414e70ac84
GRdevelops <gianniruggieri99@gmail.com>
2023-11-25 16:06:49 +0100

    serveless implementation

---
+++ api/generate-workout.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    console.log("Received data:", req.body);
+
+    try {
+      const response = await axios.post("https://api.openai.com/v1/chat/completions", {
+        model: "gpt-3.5-turbo-1106",
+        messages: [{ role: "user", content: req.body.message }],
+        temperature: 0.8,
+        max_tokens: 500,
+      }, {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      res.send(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(`Internal Server Error: ${error.message}`);
+    }
+    
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+};
